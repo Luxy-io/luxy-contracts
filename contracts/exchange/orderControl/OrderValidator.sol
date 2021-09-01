c67a6f3984a1517087672cc5629522a91aec94b1
claudiovb <claudiocarvalhovilasboas@gmail.com>
2021-09-01 17:34:24 -0300

    Adding orderValidation tests

---
+++ contracts/exchange/orderControl/OrderValidator.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./LibOrder.sol";
+import "../lib/LibSignature.sol";
+import "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/draft-EIP712Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/interfaces/IERC1271Upgradeable.sol";
+
+abstract contract OrderValidator is Initializable, ContextUpgradeable, EIP712Upgradeable {
+    using LibSignature for bytes32;
+    using AddressUpgradeable for address;
+    
+    bytes4 constant internal MAGICVALUE = 0x1626ba7e;
+
+    function __OrderValidator_init_unchained() internal initializer {
+        __EIP712_init_unchained("Exchange", "2");
+    }
+
+    function validate(LibOrder.Order memory order, bytes memory signature) internal view {
+        if (order.salt == 0) {
+            require(_msgSender() == order.maker, "maker is not tx sender");
+        } else {
+            if (_msgSender() != order.maker) {
+                bytes32 hash = LibOrder.hash(order);
+                if (_hashTypedDataV4(hash).recover(signature) != order.maker) {
+                    if (order.maker.isContract()) {
+                        require(
+                            IERC1271Upgradeable(order.maker).isValidSignature(_hashTypedDataV4(hash), signature) == MAGICVALUE,
+                            "contract order signature verification error"
+                        );
+                    } else {
+                        revert("order signature verification error");
+                    }
+                }
+            }
+        }
+    }
+
+    uint256[50] private __gap;
+}
---
+++ test/Exchange/OrderValidator.test.js
@@ -0,0 +1,42 @@
+const order = require("../order");
+const { expect } = require('chai');
+const sign = order.sign;
+const { BN, constants, expectRevert } = require('@openzeppelin/test-helpers');
+const { ZERO_ADDRESS } = constants;
+
+describe ('OrderValidator', function(){ 
+    beforeEach(async () => {
+        accounts = await ethers.provider.listAccounts();
+        const OrderValidatorTest = await ethers.getContractFactory('OrderValidatorTest');
+        lib = await OrderValidatorTest.deploy();
+    });
+
+    context('Validating Signatures', function(){ 
+        it("should validate if signer is correct", async () => {
+            const testOrder = order.Order(accounts[1], order.Asset("0xffffffff", "0x", 100), ZERO_ADDRESS, order.Asset("0xffffffff", "0x", 200), 1, 0, 0, "0xffffffff", "0x");
+            const signature = await getSignature(testOrder, accounts[1]);
+            await lib.validateOrderTest(testOrder, signature);
+        });
+    
+        it("should fail validate if signer is incorrect", async () => {
+            const testOrder = order.Order(accounts[1], order.Asset("0xffffffff", "0x", 100), ZERO_ADDRESS, order.Asset("0xffffffff", "0x", 200), 1, 0, 0, "0xffffffff", "0x");
+            const signature = await getSignature(testOrder, accounts[2]);
+            await expectRevert(
+                lib.validateOrderTest(testOrder, signature),
+                "order signature verification error"
+            );
+        });
+    
+        it("should bypass signature if maker is msg.sender", async () => {
+            const testOrder = order.Order(accounts[5], order.Asset("0xffffffff", "0x", 100), ZERO_ADDRESS, order.Asset("0xffffffff", "0x", 200), 1, 0, 0, "0xffffffff", "0x");
+            await lib.validateOrderTest(testOrder, "0x", { from: accounts[5] });
+        });
+    
+        async function getSignature(order, signer) {
+            return sign(order, signer, lib.address);
+        }
+
+    });
+
+
+});
